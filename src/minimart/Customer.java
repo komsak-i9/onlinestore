132c3ba1b2ca97a1b3a2c2ef05ed0ce9bcb3e185
ARIT <ARIT@ARIT-5Y5ZPR1.arit.co.th>
2017-07-18 11:25:45 +0700

    hello customer

--- src/minimart/Customer.java
+++ src/minimart/Customer.java
@@ -3,8 +3,10 @@ package minimart;
 public class Customer {
     
     public static void main(String[] args){
+        System.out.println("Good moning Customer");
+        
         OnlineShoppingStore onlineStore  = new OnlineShoppingStore();
-
+        
         onlineStore.showItems();
         onlineStore.getInputFromCustomer();
         
